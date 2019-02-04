31e86d107636ac8d7502f16e49fadaaa6a99a699
Carlos Anderson <carlosja@med.umich.edu>
2019-02-04 12:58:09 -0500

    Use decimal to avoid floating point comparisons

--- ZoomWpfView/ViewModel.cs
+++ ZoomWpfView/ViewModel.cs
@@ -1,33 +1,33 @@
-﻿using System;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 
 namespace ZoomWpfView
 {
-    public class ViewModel : ViewModelBase
+public class ViewModel : ViewModelBase
+{
+    private const decimal Unity = 1;
+
+    private decimal _scale = Unity;
+    public decimal Scale
     {
-        private const double Tolerance = 0.0001;
-        private const double Unity = 1.0;
-
-        private double _scale = Unity;
-        public double Scale
-        {
-            get => _scale;
-            set => Set(ref _scale, value);
-        }
-
-        public double ScaleStep => 0.1;
-
-        public double MinimumScale => 0.1;
-
-        public double MaximumScale => 4.0;
-
-        public ICommand ZoomInCommand => new RelayCommand(
-            () => Scale += ScaleStep, () => MaximumScale - Scale > Tolerance);
-        public ICommand ZoomOutCommand => new RelayCommand(
-            () => Scale -= ScaleStep, () => Scale - MinimumScale > Tolerance);
-        public ICommand ResetZoomCommand => new RelayCommand(
-            () => Scale = Unity, () => Math.Abs(Scale - Unity) > Tolerance);
+        get => _scale;
+        set => Set(ref _scale, value);
     }
+
+    public decimal ScaleStep => 0.1m;
+
+    public decimal MinimumScale => 0.1m;
+
+    public decimal MaximumScale => 4.0m;
+
+    public ICommand ZoomInCommand => new RelayCommand(
+        () => Scale += ScaleStep, () => Scale < MaximumScale);
+
+    public ICommand ZoomOutCommand => new RelayCommand(
+        () => Scale -= ScaleStep, () => Scale > MinimumScale);
+
+    public ICommand ResetZoomCommand => new RelayCommand(
+        () => Scale = Unity, () => Scale != Unity);
+}
 }
